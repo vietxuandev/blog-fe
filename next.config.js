056aa88862a7582004acd9b7802c29a6098694cd
Xuan Nguyen Viet <vietxuandev@gmail.com>
2024-04-07 19:21:30 +0700

    add dockerfile

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'strapi-iurp.onrender.com'],
+    domains: ['localhost', 'api.datvanguoibariavungtau.com'],
   },
 };
 
